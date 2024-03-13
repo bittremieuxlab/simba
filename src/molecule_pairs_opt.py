f627d8fcfd95148e677b74d050d066546706dee5
chevi1989 <sebastian.piedrahita@gmail.com>
2024-03-13 15:25:23 +0100

    molecule pairs opt

--- src/molecule_pairs_opt.py
+++ src/molecule_pairs_opt.py
@@ -20,3 +20,22 @@ class MoleculePairsOpt(MolecularPairsSet):
         self.indexes_tani = MolecularPairsSet.adjust_data_format(
             np.array(indexes_tani_unique)
         )
+
+    
+    def __add__(self, other):
+        # only to be used when the spectrums are the same
+
+        if self.are_spectrums_the_same(self.spectrums_original, other.spectrums_original):
+            new_indexes_tani = np.concatenate(
+                (self.indexes_tani, other.indexes_tani), axis=0
+            )
+            return MoleculePairsOpt(
+                spectrums_unique=self.spectrums,
+                spectrums_original=self.spectrums_original,
+                indexes_tani_unique=new_indexes_tani,
+                df_smiles=self.df_smiles,
+            )
+        else:
+            print("ERROR: Attempting to add 2 set of spectrums with different content")
+            return 0
+        
