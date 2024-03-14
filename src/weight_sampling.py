46b9f743d4ab14ab18e91aa2f0019004bbddb2d6
chevi1989 <sebastian.piedrahita@gmail.com>
2024-03-14 11:59:54 +0100

    fixing sampling

--- src/weight_sampling.py
+++ src/weight_sampling.py
@@ -10,20 +10,24 @@ class WeightSampling:
     @staticmethod
     def compute_weights(binned_list):
         freq = np.array([len(r) for r in binned_list])
-        weights = np.sum(freq) / freq
 
         # remove 1 from using the last bin for sim=1
-
         index_half = int((len(binned_list) - 1) / 2)
+
+        #for the ranges that are lower than 0.5 treat them as an only range
+        sum_freq_low_range=sum(freq[0:index_half])
+        freq_low_range= sum_freq_low_range/(len(freq[0:index_half]))
+        freq_low_range[0:index_half] = freq_low_range
+
+        weights = np.sum(freq) / freq
+
+        
 
         # deprecated
         # for the ranges that are lower than 0.5, put half the weight on the highest range
         #weights[0:index_half] = 0.5 * weights[0:index_half]
 
-        #for the ranges that are lower than 0.5 treat them as an only range
-        sum_weights_low_range=sum(weights[0:index_half])
-        weight_low_range= sum_weights_low_range/(len(weights[0:index_half]))
-        weights[0:index_half] = weight_low_range
+        
 
         # normalize the weights
         weights = weights / np.sum(weights)
