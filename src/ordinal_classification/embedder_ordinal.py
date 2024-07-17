6b2b835a45db0990dfb0ac6994d9dd217614e86e
sebastian piedrahita <sebas@sebastians-MacBook-Pro.local>
2024-07-17 16:05:32 +0200

    cross entropy adjusted

--- src/ordinal_classification/embedder_ordinal.py
+++ src/ordinal_classification/embedder_ordinal.py
@@ -26,6 +26,73 @@ from src.config import Config
 from src.transformers.embedder import Embedder
 from src.ordinal_classification.ordinal_classification import OrdinalClassification
 
+
+class CustomizedCrossEntropyLoss(nn.Module):
+    def __init__(self, n_classes=6):
+        super(CustomizedCrossEntropyLoss, self).__init__()
+        penalty_matrix = [[20, 4, 3, 2, 1, 0,],
+                          
+                          [4, 20, 4, 3, 2, 1,] ,
+
+                          [3, 4, 20, 4, 3, 2,],
+
+                          [2, 3, 4,20, 4, 3,],
+
+                          [1, 2, 3, 4, 20, 4,],
+
+                          [0, 1, 2, 3, 4, 20,]]
+        
+        #normalize:
+        #penalty_matrix = penalty_matrix/(np.sum(penalty_matrix, axis=0))
+        
+        #penalty_matrix = [[0, 1, 2, 3, 4, 5,],
+        #                  [1, 0, 1, 2, 3, 4,],
+        #                  [2, 1 ,0, 1, 2, 3,],
+        #                  [3, 2, 1, 0, 1, 2,],
+        #                  [4, 3, 2, 1, 0, 1,],
+        #                  [5, 4, 3, 2, 1, 0,]]
+        
+        self.n_classes=n_classes
+        self.device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.penalty_matrix = torch.tensor(penalty_matrix).to(self.device)/20
+
+    def forward(self, logits, target):
+        batch_size = logits.size(0)
+        
+        # Calculate softmax
+        #exp_logits = torch.exp(logits)
+        #softmax = exp_logits / torch.sum(exp_logits, dim=1, keepdim=True)
+        softmax = F.softmax(logits, dim=-1).to(self.device)
+        # Convert target to one-hot encoded tensor
+        #one_hot_target = torch.zeros_like(softmax)
+        #one_hot_target[torch.arange(batch_size).to(torch.int64), target.to(torch.int64)] = 1
+        
+        #one_hot_target = torch.nn.functional.one_hot(target.to(torch.int64), num_classes=self.n_classes)
+        # Calculate cross entropy loss
+
+        new_hot_target = self.penalty_matrix[target.to(torch.int64).to(self.device)]
+        cross_entropy_loss = -torch.sum(new_hot_target * torch.log(softmax + 1e-10)) / batch_size
+        
+        return cross_entropy_loss
+
+    #def forward(self, y_pred, y_true):
+    #    # Ensure probabilities are not zero to avoid log(0)
+    #    #y_pred = torch.clamp(y_pred, 1e-15, 1 - 1e-15)
+        
+        # Convert the true labels to one-hot encoding
+        #print(y_true.shape)
+    #    y_true = y_true.view(-1).to(torch.int64)
+        
+        
+    #    y_true_one_hot = torch.nn.functional.one_hot(y_true, num_classes=self.n_classes)
+        
+    #    # Compute the customized cross entropy loss
+    #    #loss = -torch.sum(y_true_one_hot.to(torch.float32) * torch.log(y_pred) * self.penalty_matrix, dim=1).mean()
+    #    loss = -torch.sum(y_true_one_hot.to(torch.float32) * torch.log(y_pred) * self.penalty_matrix, dim=1).mean()
+    #    # Average the loss over all samples
+    #    return loss
+    
+
 class EmbedderOrdinal(Embedder):
     """It receives a set of pairs of molecules and it must train the similarity model based on it. Embed spectra."""
 
@@ -59,7 +126,7 @@ class EmbedderOrdinal(Embedder):
 
         self.loss_fn = nn.CrossEntropyLoss()
         self.regression_loss = nn.MSELoss()
-
+        self.customised_ce = CustomizedCrossEntropyLoss()
 
         self.dropout = nn.Dropout(p=dropout)
         self.use_gumbel=use_gumbel
@@ -115,10 +182,11 @@ class EmbedderOrdinal(Embedder):
 
         # the sim data is received in the range 0-1
         target = torch.tensor(batch["similarity"]).to(self.device)
-        target = target.view(-1).long()  # Ensure targets are in the right shape and type for classification
+        target = target.view(-1).float()  # Ensure targets are in the right shape and type for classification
 
         
-        loss = self.loss_fn(logits, target)
+        #loss = self.loss_fn(logits, target)
+        loss =self.customised_ce(logits, target)
         # Compute the expected value (continuous) from probabilities
         #expected_value = torch.sum(probs * torch.arange(logits.size(1)).to(self.device), dim=1)
 
@@ -147,20 +215,31 @@ class EmbedderOrdinal(Embedder):
     def step_mse(self, batch, batch_idx, threshold=0.5):
         """A training/validation/inference step."""
         logits = self(batch)
-        logits = self.gumbel_softmax(logits)
+        #print('***')
+        #print('before softmax')
+        #print(logits[0])
+        logits = F.softmax(logits, dim=-1)
+        #logits = self.gumbel_softmax(logits)
+        
+        #print('after softmax')
+        #print(logits[0])
+        
         target = torch.tensor(batch["similarity"]).to(self.device)
         target = target.view(-1).float()  # Ensure targets are in the right shape and type for regression
-
+        #print('target')
+        #print(target[0])
         
         # Compute the probabilities from logits using softmax
         #probabilities = torch.nn.functional.softmax(logits, dim=1)
 
         # Compute the expected value (continuous) from probabilities
-        expected_value = torch.sum(logits * torch.arange(logits.size(1)).to(self.device), dim=1)
-
-        
+        predicted_value = torch.sum(logits * torch.arange(logits.size(1)).to(self.device), dim=1)
+        #print('inference')
+        #print(expected_value[0])
         # Compute the MSE between the expected value and the target
-        loss = self.regression_loss(expected_value, target)
+        #print('loss')
+        loss = self.regression_loss(predicted_value, target)
+        #print(loss)
 
         #print('example:')
         #print(logits[0])
@@ -171,7 +250,7 @@ class EmbedderOrdinal(Embedder):
         return loss
     
     
-    def gumbel_softmax(self, logits, temperature=0.5, hard=True):
+    def gumbel_softmax(self, logits, temperature=0.2, hard=True):
             return F.gumbel_softmax(logits, tau=temperature, hard=hard)
     
 
