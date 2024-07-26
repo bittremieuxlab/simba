07f5fcba1d1fb95ef909f41009c360cd18617d39
chevi1989 <sebastian.piedrahita@gmail.com>
2024-07-26 10:25:19 +0200

    edit

---
+++ src/edit_distance/load_edit_distance_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import subprocess
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+from rdkit import Chem
+
+
+class LoadEditDistanceData:
+    '''
+    class to load data from ming processing
+    '''
