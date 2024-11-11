285a66bccbcc414f027b352a9eeda03b69c9e7a2
sebastian piedrahita <sebas@sebastians-MacBook-Pro.local>
2024-11-11 15:48:24 +0100

    local

--- script_transformers_multitasking.sh
+++ script_transformers_multitasking.sh
@@ -8,7 +8,7 @@ export PATH="${VSC_DATA}/miniconda3/bin:${PATH}"
 source activate transformers
 
 nvidia-smi 
-srun python training_multitasking.py --enable_progress_bar=0   --extra_info=_multitasking_penaltymatrix_20241029
+srun python training_multitasking.py --enable_progress_bar=0   --extra_info=_multitasking_nologmces --USE_MCES20_LOG_LOSS=0
 #--enable_progress_bar=0 --PREPROCESSING_DIR=/scratch/antwerpen/209/vsc20939/data/preprocessing_mces_threshold20_newdata_20240925/ --extra_info=_multitasking_mces20raw_newdata
 #--EDIT_DISTANCE_USE_GUMBEL=1
 #--D_MODEL=128  
