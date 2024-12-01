e16e0c400b9ea4efeb9a558578a6ed4c21f1656a
chevi1989 <sebastian.piedrahita@gmail.com>
2024-12-01 19:38:20 +0100

    augmentations

--- script_inference_multitasking.sh
+++ script_inference_multitasking.sh
@@ -13,7 +13,7 @@ nvidia-smi
 #srun python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_contrastive --USE_MCES20_LOG_LOSS=0 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=1 --BEST_MODEL_NAME=last.ckpt
 #srun python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_multitasking_contrastive --USE_MCES20_LOG_LOSS=0 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=1 --LR=0.00001 --BEST_MODEL_NAME=last.ckpt
 #srun  python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_multitasking_maxp20_ed_improved --USE_MCES20_LOG_LOSS=1 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=1 --load_pretrained=1 --LR=0.00001
-srun  python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_multitasking_maxp40 --USE_MCES20_LOG_LOSS=1 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=1 --load_pretrained=1 --LR=0.00001
+srun  python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_multitasking_augmentations --USE_MCES20_LOG_LOSS=1 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=1 --load_pretrained=1 --LR=0.00001
 
 #srun python inference_multitasking.py --enable_progress_bar=0   --extra_info=_multitasking_precursor_augmentation --USE_MCES20_LOG_LOSS=1 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=0 
 #srun python inference_multitasking.py  --enable_progress_bar=0   --extra_info=_multitasking_precursor_randomized --USE_MCES20_LOG_LOSS=1 --use_cosine_distance=1 --USE_LOSS_WEIGHTS_SECOND_SIMILARITY=0 
--- src/transformers/augmentation.py
+++ src/transformers/augmentation.py
@@ -1,6 +1,6 @@
 import random
 import copy
-
+import numpy as np
 
 class Augmentation:
 
@@ -14,7 +14,7 @@ class Augmentation:
         new_sample = Augmentation.normalize_max(new_sample)
         new_sample = Augmentation.peak_augmentation_removal_noise(data_sample)
         new_sample = Augmentation.peak_augmentation_max_peaks(data_sample,)
-        
+
         #precursor mass
         new_sample = Augmentation.add_false_precursor_masses_negatives(new_sample)
         new_sample = Augmentation.add_false_precursor_masses_positives(new_sample)
@@ -31,76 +31,80 @@ class Augmentation:
             mz_column = 'mz' + sufix 
 
             # normalization
-            intensity = new_sample[intensity_column]
-            mz = new_sample[mz_column]
-            intensity = intensity / np.max(intensity, axis=1, keepdims=True)
+            intensity = np.array(data_sample[intensity_column])
+            mz = data_sample[mz_column]
+            
+            intensity = intensity / np.max(intensity,  keepdims=True)
 
             # apply 
-            new_sample[intensity_column] = intensity
-            new_sample[mz_column] = mz
+            data_sample[intensity_column] = intensity
+            data_sample[mz_column] = mz
 
         return data_sample
 
 
     @staticmethod
-    def peak_augmentation_max_peaks(data_sample, p_augmentation=1.0, max_peaks=100):
+    def peak_augmentation_max_peaks(data_sample, p_augmentation=0.5, max_peaks=100):
          # first normalize to maximum
 
-        
-        max_augmented_peaks= max(20, random.random()*max_peaks)
+        if random.random()<p_augmentation:
+            max_augmented_peaks= int(max(20, random.random()*max_peaks))
 
-        for sufix in ['_0','_1']:
-            #put a threshold
-            intensity_column= 'intensity' + sufix
-            mz_column = 'mz' + sufix 
-            intensity = new_sample[intensity_column]
-            mz = new_sample[mz_column]
+            for sufix in ['_0','_1']:
+                #put a threshold
+                intensity_column= 'intensity' + sufix
+                mz_column = 'mz' + sufix 
+                intensity = data_sample[intensity_column]
+                mz = data_sample[mz_column]
 
-            # order intensities by amplitude
-            intensity_ordered_indexes = np.argsort(intensity, axis=1)[:,::-1]  # flip the order to have the max at the beginning
-            indexes_to_be_erased = intensity_ordered_indexes[:,max_augmented_peaks:-1]
-            
-            for row_index,(row_indexes_to_be_erased, intensities) in enumerate(indexes_to_be_erased, intensity):
-                intensity[row_index, row_indexes_to_be_erased]=0
-                mz[row_index, row_indexes_to_be_erased]=0
+                # order intensities by amplitude
+                intensity_ordered_indexes = np.argsort(intensity)[::-1]  # flip the order to have the max at the beginning
+                indexes_to_be_erased = intensity_ordered_indexes[max_augmented_peaks:-1]
+                
+                intensity[indexes_to_be_erased]=0
+                mz[indexes_to_be_erased]=0
 
-            # apply 
-            new_sample[intensity_column] = intensity
-            new_sample[mz_column] = mz
-
-        return new_sample
+                # apply 
+                data_sample[intensity_column] = intensity
+                data_sample[mz_column] = mz
+            return data_sample
+        else:
+            return data_sample
 
 
     @staticmethod
-    def peak_augmentation_removal_noise(data_sample, max_percentage=0.01):
+    def peak_augmentation_removal_noise(data_sample, max_percentage=0.01, p_augmentation=0.5):
 
-        # first normalize to maximum
-        for sufix in ['_0','_1']:
-            #put a threshold
-            intensity_column= 'intensity' + sufix
-            mz_column = 'mz' + sufix 
-            intensity = new_sample[intensity_column]
-            mz = new_sample[mz_column]
+        if random.random()<p_augmentation:
+            # first normalize to maximum
+            for sufix in ['_0','_1']:
+                #put a threshold
+                intensity_column= 'intensity' + sufix
+                mz_column = 'mz' + sufix 
+                intensity = data_sample[intensity_column]
+                mz = data_sample[mz_column]
 
-            # remove noise peaks
-            max_amplitude= np.random()* max_percentage
-            intensity[intensity < max_amplitude] = 0
-            mz[intensity < max_amplitude] = 0
+                # remove noise peaks
+                max_amplitude= random.random()* max_percentage
+                intensity[intensity < max_amplitude] = 0
+                mz[intensity < max_amplitude] = 0
 
-            # apply 
-            new_sample[intensity_column] = intensity
-            new_sample[mz_column] = mz
-        # put a threshold based on peak amplitude
+                # apply 
+                data_sample[intensity_column] = intensity
+                data_sample[mz_column] = mz
+            # put a threshold based on peak amplitude
 
-        return data_sample
+            return data_sample
+        else:
+            return data_sample
 
     @staticmethod 
-    def normalize_intensities(data_sample)
+    def normalize_intensities(data_sample):
         # sqrt root
         for intensity_column in ['intensity_0','intensity_1']:
             intensity = data_sample[intensity_column]
             intensity = np.sqrt(intensity)
-            intensity = intensity / np.sqrt(np.sum(intensity**2, axis=1, keepdims=True))
+            intensity = intensity / np.sqrt(np.sum(intensity**2,  keepdims=True))
             data_sample[intensity_column]= intensity
         return data_sample
          
@@ -124,23 +128,12 @@ class Augmentation:
         new_sample["similarity"] = data_sample["similarity"]
         return new_sample
 
-    @staticmethod
-    def add_noise_to_precursor_mass(sample, max_noise=1.0):
-
-        added_noise_factor_0 = random.uniform(-max_noise, max_noise)
-        added_noise_factor_1 = random.uniform(-max_noise, max_noise)
-        sample["precursor_mass_0"] = sample[
-            "precursor_mass_0"
-        ] + added_noise_factor_0 * (sample["precursor_mass_0"])
-        sample["precursor_mass_1"] = sample[
-            "precursor_mass_1"
-        ] + added_noise_factor_1 * (sample["precursor_mass_1"])
-        return sample
+  
 
     @staticmethod
     def add_false_precursor_masses_positives(sample, max_noise=0.01, p_augmentation=0.2):
         '''
-        create a pair where the precursor masses are almost the same
+        create a pair where the precursor masses are very different
         '''
 
         if random.random()< p_augmentation:
